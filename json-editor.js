fa33f3fe25e7cb8117846ea3dd607dcd50f30504
Wei Sun <wwsun@CNwwsun.local>
2017-08-24 11:28:33 +0800

    for 2.0, compress and store in internet; Wayde

--- json-editor.js
+++ json-editor.js
@@ -3,61 +3,117 @@
 var program = require('commander');
 var fs = require('fs');
 var path = require('path');
-var base64 = require('base-64');
+var jsonPack = require('jsonpack');
+var request = require('request');
+var ProgressBar = require('progress');
 var rootURL = 'http://www.jsoneditoronline.cn/';
+// var rootURL = 'http://localhost/';
 
+
+var bar = new ProgressBar('  uploading [:bar] :rate/bps :percent :etas', {
+  complete: '=',
+  incomplete: ' ',
+  width: 100,
+  total: 20
+});
 
 function isUrl(fileString) {
   return fileString.indexOf('http://') === 0 || fileString.indexOf('https://') === 0;
 }
 
+function readJsonFile(file, cb) {
+  var lastSlash = file.lastIndexOf('/');
+  var filename = '';
+  var filePath = '';
+
+  if(lastSlash === -1){
+    filename = file;
+    filePath = path.join(process.cwd(), filename);
+  } else {
+    filePath = path.resolve(file);
+  }
+
+  console.log('Open file in the path:');
+  console.log(filePath + '\n');
+
+  fs.readFile(filePath, "utf8", function read(err, data) {
+    if (err) {
+      throw err;
+    } else {
+      cb(JSON.parse(data));
+    }
+  });
+}
+
+function storeMyJson(jsonData, cb) {
+  request.post(
+    'https://api.myjson.com/bins',
+    { json: jsonData },
+    function (error, response, body) {
+      if (!error && response.statusCode == 201) {
+        bar.update(1, {});
+        console.log('\ncomplete\n');
+        cb(body.uri);
+      } else {
+        console.log('\nFail, please retry later.\n');
+      }
+      clearInterval(timer);
+    }
+  );
+
+  var timer = setInterval(function () {
+    bar.tick(1, {});
+    if (bar.complete) {
+      clearInterval(timer);
+    }
+  }, 1000);
+}
+
+function printReminder(url) {
+  console.log("If browser not opened in 5s, you also can copy the link below and paste it into browser address bar:");
+  console.log('------======------');
+  console.log(url);
+  console.log('------======------');
+}
+
 program
   .arguments('<file>')
-  .option('-u, --username <username>', 'The user to authenticate as')
-  .option('-p, --password <password>', 'The user\'s password')
-  .option('-b, --browser', 'Open the json file in the system browser')
+  .option('-w, --www', 'Store json file on internet, and open by url')
   .action(function(file) {
 
     if(isUrl(file)){
       var url = rootURL + '?url=' + file;
+      printReminder(url);
       require('child_process').spawn('open', [url]);
       return;
     }
 
-    var lastSlash = file.lastIndexOf('/');
-    var filename = '';
-    var filePath = '';
-
-    if(lastSlash === -1){
-      filename = file;
-      filePath = path.join(process.cwd(), filename);
+    if(program.www){
+      readJsonFile(file, function (jsonObj) {
+        storeMyJson(jsonObj, function (url) {
+          var url = rootURL + '?url=' + url;
+          printReminder(url);
+          require('child_process').spawn('open', [url]);
+        });
+      });
     } else {
-      filePath = path.resolve(file);
-    }
-
-
-    console.log('Open file in the path:');
-    console.log(filePath);
-
-    fs.readFile(filePath, "utf8", function read(err, data) {
-      if (err) {
-        throw err;
-      } else {
-        var content = data;
-        var encoded = base64.encode(content);
-
+      readJsonFile(file, function (jsonObj) {
+        var encoded = jsonPack.pack(jsonObj);
         var url = rootURL + '?data=' + encoded;
 
-        console.log("If browser not opened in 5s, you also can copy the link below and paste it into browser address bar:");
-        console.log('------======------');
-        console.log(url);
-        console.log('------======------');
+        if(url.length > 8192){
+          storeMyJson(jsonObj, function (url) {
+            url = rootURL + '?url=' + url;
+            printReminder(url);
+            require('child_process').spawn('open', [url]);
+          });
+        } else {
+          printReminder(url);
+          require('child_process').spawn('open', [url]);
+        }
 
-        require('child_process').spawn('open', [url]);
-      }
-    });
-
-
+      });
+    }
   })
   .parse(process.argv);
 
